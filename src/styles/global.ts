510a1c50917041a8bca193c26e086beefc844b8e
Awais Ahmad <thehappyprogramer1@gmail.com>
2026-06-18 09:10:10 +0500

    fix gloable style issues

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle`
+const GlobalStyle = createGlobalStyle`
   :root{
     --pink: #E31F71;
     --black: #212121;
@@ -115,4 +115,6 @@ export const GlobalStyle = createGlobalStyle`
     //   color: var(--green);
     // }
   }
-`
+`;
+
+export default GlobalStyle;
